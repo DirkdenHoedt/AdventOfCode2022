d02028c7a51cdebc99cd11e7e9fe245be654e898
Dirk den Hoedt <dirk.denhoedt@alten.nl>
2022-12-02 10:41:33 +0100

    day2

---
+++ day2/part1.py
@@ -0,0 +1,30 @@
+score = 0
+
+with open('day2/input.txt') as f:
+    for l in f:
+        l = l.strip()
+        opp = l[0]
+        me = l[2]
+        if opp == 'A':
+            if me == 'X':
+                score += 3 + 1
+            elif me == 'Y':
+                score += 6 + 2
+            elif me == 'Z':
+                score += 0 + 3
+        elif opp == 'B':
+            if me == 'X':
+                score += 0 + 1
+            elif me == 'Y':
+                score += 3 + 2
+            elif me == 'Z':
+                score += 6 + 3
+        elif opp == 'C':
+            if me == 'X':
+                score += 6 + 1
+            elif me == 'Y':
+                score += 0 + 2
+            elif me == 'Z':
+                score += 3 + 3
+
+print(score)
